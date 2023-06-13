5cc86a03078fbca7ad51ccbd281f93a9b5b16a43
ManuelF <manuel.floriani1999@gmail.com>
2023-06-13 10:55:34 +0200

    Aggiunto componente header

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,98 @@
+<script>
+
+export default {
+    name : "PageHeader",
+}
+</script>
+
+<template>
+    <header>
+        <section class="container-fluid d-flex justify-content-between">
+            <div class="row align-items-center justify-content-between">
+                
+                <div class="logo col">
+                    <img src="../assets/images/gym_logo_2x.png" alt="">
+                </div>
+                <nav class="col-6">
+                    <ul class="nav justify-content-end">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">ABOUT US</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">FACILITIES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">MEMBERSHIP</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">TESTIMONIALS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">BLOG</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            
+        </section>
+        <section class="jumbotron text-center">
+            <h1 class="text-center mt-5">NO PAIN NO GAIN</h1>
+            <p class="mt-3">Motivation is what gets you started. Habit is what keeps you going.</p>
+            <button class="mt-3 py-3 px-5">JOIN US TODAY</button>
+        </section>
+    </header>
+
+
+</template>
+
+<style scoped lang="scss">
+@use '../styles/general.scss';
+@use '../styles/partials/variables.scss';
+@import '../styles/partials/variables';
+
+header{
+    background-image: url(../assets/images/home-banner.jpg) ;
+    background-size: contain;
+    height: 800px;
+    .row{
+        width: 100%;
+    }
+    .logo{
+        img{
+            width: 300px;
+        }
+
+    }
+    li{
+        color: $quarto;
+        font-size: 14px;
+        .active{
+            color: $primo;
+        }
+    }
+    .jumbotron{
+        margin-top: 140px;
+        color: $quarto;
+        
+        h1{
+            font-size: 80px;
+            font-weight: 700;
+        }
+
+        p{
+            font-weight: 200;
+        }
+
+        button{
+            font-weight: 200;
+            color: $quarto;
+            background-color: transparent;
+            border: 1px solid $quarto;
+        }
+    }
+}
+
+</style>
