34fd85f8739698d4b7b263490fe9554ce8140355
ManuelF <manuel.floriani1999@gmail.com>
2023-06-13 22:45:31 +0200

    Creato componente PageResults

--- src/components/PageResultsCard.vue
+++ src/components/PageResultsCard.vue
@@ -6,65 +6,74 @@ export default {
 </script>
 
 <template>
+    <div class="container-fluid">
+        <div class="title">
+            <h2>REAL WORLD RESULTS</h2>
+            <img src="../assets/images/divider-xx-red.png" alt="">
+        </div>
+        <div class="row riga row-cols-1 row-cols-md-2 g-4">
+            <div class="col">
+            <div class="card">
+                <div class="row align-items-center g-0">
+                    <div class="card-img col-4">
+                    <img src="../assets/images/review_1-compressor.jpg" class="card-img-top" alt="...">
+                </div>
+            <div class="card-body col-4">
+                <em class="card-text">"The trainers at Avada Gym have helped me rach my fitness goals beyond anything I could of hoped for. I love it!"</em>
+                <p>- <b>Tara Smith</b></p>
+            </div>
+                </div>
+                
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <div class="row align-items-center g-0">
+                    <div class="card-img col-4">
+                    <img src="../assets/images/review_3-compressor-2.jpg" class="card-img-top" alt="...">
+                </div>
+            <div class="card-body col-4">
+                <em class="card-text">"The atmosphere at Avada Gym is amazing, the people are even better. All in all, it is a great place to work out."</em>
+                <p>-<b>Simon Chel</b></p>
+            </div>
+                </div>
+                
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <div class="row align-items-center g-0">
+                    <div class="card-img col-4">
+                    <img src="../assets/images/review_2-compressor.jpg" class="card-img-top" alt="...">
+                </div>
+            <div class="card-body col-4">
+                <div class="testo">
 
-<div class="row row-cols-1 row-cols-md-2 g-4">
-    <div class="col">
-    <div class="card">
-        <div class="row g-0">
-            <div class="card-img col-4">
-            <img src="../assets/images/review_4-compressor-2.jpg" class="card-img-top" alt="...">
+                    <em class="card-text">"The facilities at Avada Gym stood out to me the most. They are well maintainer and have high quality equipment."</em>
+                    <p>-<b>Jen Wirth</b></p>
+
+                </div>
+            </div>
+                </div>
+                
+            </div>
         </div>
-      <div class="card-body col-4">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-      </div>
+        <div class="col">
+            <div class="card">
+                <div class="row g-0">
+                    <div class="card-img col-4">
+                    <img src="../assets/images/review_4-compressor-2.jpg" class="card-img-top" alt="...">
+                </div>
+            <div class="card-body align-items-center col-4">
+                <em class="card-text">"No bad things to say about Avada Gym, they are top notch at every corner and truly take care of their costumers"</em>
+                <p>- <b>Jeff Glum</b></p>
+            </div>
+                </div>
+                
+            </div>
         </div>
-        
+        </div>
     </div>
-  </div>
-  <div class="col">
-    <div class="card">
-        <div class="row g-0">
-            <div class="card-img col-4">
-            <img src="../assets/images/review_4-compressor-2.jpg" class="card-img-top" alt="...">
-        </div>
-      <div class="card-body col-4">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-      </div>
-        </div>
-        
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-        <div class="row g-0">
-            <div class="card-img col-4">
-            <img src="../assets/images/review_4-compressor-2.jpg" class="card-img-top" alt="...">
-        </div>
-      <div class="card-body col-4">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-      </div>
-        </div>
-        
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-        <div class="row g-0">
-            <div class="card-img col-4">
-            <img src="../assets/images/review_4-compressor-2.jpg" class="card-img-top" alt="...">
-        </div>
-      <div class="card-body col-4">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-      </div>
-        </div>
-        
-    </div>
-  </div>
-</div>
 
 </template>
 
@@ -73,20 +82,43 @@ export default {
 @use '../styles/partials/variables.scss';
 @import '../styles/partials/variables';
 
-.card-img{
-    width: 150px;
-    img{
-        padding: 20px;
-        width: 150px;
-        height: 150px;
+.container-fluid{
+    background-color: $secondo;
+    color: $quarto;
+    padding-bottom: 100px;
+    .title{
+        text-align: center;
+        padding-top: 100px;
+        padding-bottom: 20px;
+        h2{
+            padding-bottom: 25px;
+            font-weight: 700
+        }
     }
-}
-.card-body{
-    margin-top: 20px;
-    width: calc(100% - 150px);
-    font-size: 10px;
-    padding: 0;
-    h5{
+    .riga{
+        width: 80%;
+        margin: 0 auto;
+        .card{
+            background-color: $terzo;
+            .card-img{
+                width: 150px;
+                img{
+                    padding: 20px;
+                    width: 150px;
+                    height: 150px;
+                }
+            }
+            .card-body{
+                margin-top: 20px;
+                width: 60%;
+                font-size: 13px;
+                padding: 0;
+                padding-right: 40px;
+                line-height: 25px;
+
+
+            }
+        }
     }
 }
 </style>
